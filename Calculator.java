9fb4044ee2be31ee13b97bbf64ba7f620b945a8b
Cestmir Houska <czestmyr@gmail.com>
2012-03-14 16:44:19 +0100

    TEST: More things from Configurator tested
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,43 @@
 import java.lang.System.*;
+import java.lang.annotation.*;
 import org.ow2.dsrg.jpmf.util.*;
+import org.ow2.dsrg.jpmf.util.Configurator.*;
 
 public class Calculator {
 	public static void main(String args[]) {
 		System.out.println("Hello, world!");
+
 		Calculator calc = new Calculator();
+		Configurator.set(calc, "magic", "41");
+		Configurator.check(calc);
+
 		int result = calc.add(2,2);
 		System.out.println("The result is: "+result);
+		calc.printDefault();
 	}
 
 	public int add(int a, int b) {
 		return a + b + magic;
 	}
 
-	int magic = 5;
+	@Setter (name = "magic")
+	public void setOddMagic(String newMagic) {
+		int magicVal = Integer.parseInt(newMagic);
+		if (magicVal % 2 == 1) {
+			magic = magicVal;
+			System.out.println("Magic was odd");
+		} else {
+			System.out.println("Magic was NOT odd");
+		}
+	}
+
+	public void printDefault() {
+		System.out.println("The default value is: " + defaultInt);
+	}
+
+	@Property (name = "magic", required = true)
+	private Integer magic;
+
+	@Property (name = "default", defaultValue = "15")
+	private Integer defaultInt;
 }
