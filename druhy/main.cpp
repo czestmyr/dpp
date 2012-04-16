aac174134399deda6e25e657d06baa2942878598
Cestmir Houska <czestmyr@gmail.com>
2012-04-16 15:21:06 +0200

    Added a test main.cpp to be able to test stuff + Makefile
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

---
+++ druhy/main.cpp
@@ -0,0 +1,25 @@
+// TODO: create a real main function. Now, it just test things...
+#include "types/Integer.h"
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	string a, b, c, d, e;
+	Integer i;
+
+	a = "1235";
+	b = "   + 1687  ";
+	c = "- 345";
+	d = "125.862";
+	e = "\t\r\n545";
+
+	cout << "\"" << a << "\" - " << ((i.conforms(a))?"conforms":"invalid") << endl;
+	cout << "\"" << b << "\" - " << ((i.conforms(b))?"conforms":"invalid") << endl;
+	cout << "\"" << c << "\" - " << ((i.conforms(c))?"conforms":"invalid") << endl;
+	cout << "\"" << d << "\" - " << ((i.conforms(d))?"conforms":"invalid") << endl;
+	cout << "\"" << e << "\" - " << ((i.conforms(e))?"conforms":"invalid") << endl;
+
+	return 0;
+}
