0ae4e5eb7e114f0a8a59080a9dc01ad5db8f22a7
Cestmir Houska <czestmyr@gmail.com>
2012-04-18 23:40:52 +0200

    Option syntax class added
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

---
+++ druhy/src/OptionSyntax.cpp
@@ -0,0 +1,54 @@
+#include "OptionSyntax.h"
+
+using namespace std;
+
+OptionSyntax::OptionSyntax(): lastId(0) {}
+
+void OptionSyntax::addOption(const std::string& optionName, IType* type) {
+	unsigned int id = getUnusedId();
+	ids.insert(pair<std::string, unsigned int>(optionName, id));
+	types.insert(pair<unsigned int, IType*>(id, type));
+	helpStrings.insert(pair<unsigned int, std::string>(id, ""));
+}
+
+bool OptionSyntax::setSynonymous(const std::string& option1, const std::string& option2) {
+	// TODO: implement
+}
+
+bool OptionSyntax::setOptionHelp(const std::string& optionName, const std::string& helpString) {
+	// TODO: implement
+}
+
+bool OptionSyntax::wasAdded(const std::string& option) const {
+	return ids.count(option) != 0;
+}
+
+const IType* OptionSyntax::getType(const std::string& option) const {
+	if (ids.count(option) == 0) {
+		return NULL;
+	}
+
+	// Find will not fail here, because we tested presence of the option
+	// in ID map and types will always have an entry for existing id
+	unsigned int id = (*ids.find(option)).second;
+	return (*types.find(id)).second;
+}
+
+const std::string& OptionSyntax::getHelp(const std::string& option) const {
+	if (ids.count(option) == 0) {
+		return emptyHelp;
+	}
+
+	// Find will not fail here, because we tested presence of the option
+	// in ID map and helpStrings will always have an entry for existing id
+	unsigned int id = (*ids.find(option)).second;
+	return (*helpStrings.find(id)).second;
+}
+
+unsigned int OptionSyntax::getUnusedId() {
+	lastId++;
+	return lastId;
+}
+
+string OptionSyntax::emptyHelp = "";
+
---
+++ druhy/src/OptionSyntax.h
@@ -0,0 +1,32 @@
+#ifndef _ARGLIB_OPTION_SYNTAX_H_
+#define _ARGLIB_OPTION_SYNTAX_H_
+
+#include <string>
+#include <map>
+#include "IType.h"
+
+class OptionSyntax {
+	public:
+		OptionSyntax();
+
+		// TODO: So far, type will be owned by the OptionSyntax. Make it better in the future
+		void addOption(const std::string& optionName, IType* type = NULL);
+		bool setSynonymous(const std::string& option1, const std::string& option2);
+		bool setOptionHelp(const std::string& optionName, const std::string& helpString);
+
+		bool wasAdded(const std::string& option) const;
+		const IType* getType(const std::string& option) const;
+		const std::string& getHelp(const std::string& option) const;
+	private:
+		unsigned int getUnusedId();
+		unsigned int lastId;
+
+		std::map<std::string, unsigned int> ids;
+		std::map<unsigned int, IType*> types;
+		std::map<unsigned int, std::string> helpStrings;
+
+		static std::string emptyHelp;
+};
+
+#endif
+
