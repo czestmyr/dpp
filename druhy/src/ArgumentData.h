c971080e45b8ebc2b5775ceb63167d3c27981619
Martin Bulant <bulisekspam@centrum.cz>
2012-04-26 14:01:03 +0200

    Prepared first test for short options

--- druhy/src/ArgumentData.h
+++ druhy/src/ArgumentData.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <vector>
+#include "Value.h"
 
 class ArgumentData {
 	public:
--- druhy/src/tests/TestCases.cpp
+++ druhy/src/tests/TestCases.cpp
@@ -1,5 +1,26 @@
 #include "TestCases.h"
 
-bool UseCaseShortOptionTest::runTest() {
+#include "../types/Integer.h"
 
+bool UseCaseShortOptionTest::runTest() {
+	ArgumentsHandler h;
+	h.addOption("v");
+	h.addSynonym("v","u");
+
+	Integer* integer = new Integer;
+	integer->setHighBound(100);
+	integer->setLowBound(0);
+	h.addOption("p", REQUIRED, integer, "Help for the integer option");
+
+	char* arg1 = "-v";
+	char* arg2 = "-u";
+	char* arg3 = "-p";
+	char* arg4 = "20";
+	char* argv[4];
+	argv[0] = arg1;
+	argv[1] = arg2;
+	argv[2] = arg3;
+	argv[3] = arg4;
+
+	h.parse(4,argv);
 }
