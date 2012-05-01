bbc05bba9f0906ef34750352a96ac650b42555d9
Cestmir Houska <czestmyr@gmail.com>
2012-05-01 12:50:43 +0200

    Fixed style in ArgumentsHandler.cpp
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- druhy/src/ArgumentsHandler.cpp
+++ druhy/src/ArgumentsHandler.cpp
@@ -5,22 +5,29 @@ ArgumentsHandler::ArgumentsHandler(){}
 void ArgumentsHandler::addOption(const std::string& optionName, ParameterAttribute attrib, IType* paramType, const std::string& helpString) {
 	syntax.addOption(optionName, attrib, paramType, helpString);
 }
+
 void ArgumentsHandler::addSynonym(const std::string& original, const std::string& synonym) {
 	syntax.addSynonym(original, synonym);
 }
+
 void ArgumentsHandler::writeHelp() {
 	//TODO: Implement
 }
+
 void ArgumentsHandler::parse(int argc, char* argv[]) {
 	ArgumentParser p(&syntax, &data);
 	p.parse(argc, argv);
 }
+
 bool ArgumentsHandler::isOptionSet(const std::string& optionName) {
 	data.isOptionSet(optionName);
 }
+
 Value ArgumentsHandler::getOptionArgument(const std::string& optionName) {
 	data.getOptionArgument(optionName);
 }
+
 const std::vector<std::string>& ArgumentsHandler::getRegularArguments() {
 	data.getArguments();
 }
+
