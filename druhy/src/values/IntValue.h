db8c1aadf2384480ca34ff129f2dd43940bdc4a5
Martin Bulant <bulisekspam@centrum.cz>
2012-05-03 18:41:43 +0200

    Added exception when dynamic_cast fail

--- druhy/src/values/IntValue.h
+++ druhy/src/values/IntValue.h
@@ -15,11 +15,15 @@ class IntValue: public Value {
 		int value;
 };
 
-//TODO: Exception!
+//TODO: Add proper information to this exception!
 template <>
 int ValueHandle::getValue<int>() {
-	IntValue* intValue = dynamic_cast<IntValue*>(valuePtr);
-	return intValue->get();
+		IntValue* intValue = dynamic_cast<IntValue*>(valuePtr);
+		if( intValue == NULL ) {
+			throw ArgumentException("Type defined with this option doesn't match type required by this function.");
+		}
+		return intValue->get();
+
 };
 
 #endif
