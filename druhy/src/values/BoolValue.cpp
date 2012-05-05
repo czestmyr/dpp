02d5103ec610ee5a47b68d59b573525172a2ca48
Martin Bulant <bulisekspam@centrum.cz>
2012-05-05 22:04:25 +0200

    Added proper Exception text, when optionparameter is queried when option doesn't have one.

--- druhy/src/values/BoolValue.cpp
+++ druhy/src/values/BoolValue.cpp
@@ -3,6 +3,10 @@
 
 //TODO: Add proper information to this exception!
 template <> bool ValueHandle::getValue<bool>() {
+		if( valuePtr == NULL ) {
+			throw ArgumentException("No parametr were added to this option.");
+		}
+
 		BoolValue* boolValue = dynamic_cast<BoolValue*>(valuePtr);
 		if( boolValue == NULL ) {
 			throw ArgumentException("Type defined with this option doesn't match type required by this function.");
