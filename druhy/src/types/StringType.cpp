5e42340bc65885f26c4a82093bccf90d1e748454
Martin Bulant <bulisek@centrum.cz>
2012-05-05 10:18:26 +0200

    Added source files for StringType and StringValue

---
+++ druhy/src/types/StringType.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include "StringType.h"
+#include "../values/Values.h"
+#include "../StringParser.h"
+
+using namespace std;
+
+StringType::StringType() {
+	
+}
+
+Value* StringType::fromString(const string& argument) const {
+	// Set and return the correct value
+	StringValue* val = new StringValue(argument);
+	return val;
+}
+
+Type* StringType::clone() const {
+	return new StringType(*this);
+}
+
--- druhy/src/values/StringValue.h
+++ druhy/src/values/StringValue.h
@@ -3,12 +3,28 @@
 
 #include <string>
 #include "../Value.h"
+#include "../ValueHandle.h"
+#include "../ArgumentException.h"
 
 class StringValue : public Value {
 	public:
-		StringValue(std::string newValue):value(newValue);
+		StringValue(std::string newValue):value(newValue){}
+		~StringValue(){}
+				void set(std::string newValue) { value = newValue; }
+		std::string get() { return value; }
 	private:
 		std::string value;
 
-}
+};
+
+//TODO: Add proper information to this exception!
+template <>
+std::string ValueHandle::getValue<std::string>() {
+		StringValue* stringValue = dynamic_cast<StringValue*>(valuePtr);
+		if( stringValue == NULL ) {
+			throw ArgumentException("Type defined with this option doesn't match type required by this function.");
+		}
+		return stringValue->get();
+
+};
 #endif
