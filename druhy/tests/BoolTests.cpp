3128d918062a3eec0d721e658ccb4b052df7ef66
Martin Bulant <bulisekspam@centrum.cz>
2012-05-05 22:06:56 +0200

    Added more tests of BoolType and BoolValue

--- druhy/tests/BoolTests.cpp
+++ druhy/tests/BoolTests.cpp
@@ -13,8 +13,9 @@ using namespace std;
 void BoolTests::prepareTests() {
 	Tests& testSet = Tests::inst();
 	testSet.addTest(&BoolTests::trueFalseTest, "Testing whether boolean values true and false are correctly handled");
-	testSet.addTest(&BoolTests::badInputTest, "Testing whether bad boolean inputs arent parsed");
-	testSet.addTest(&BoolTests::trailingCharacterTest, "Testing whether only values true or false are accepted");
+	testSet.addTest(&BoolTests::badInputTest, "Testing whether bad boolean inputs aren't parsed");
+	testSet.addTest(&BoolTests::noInputTest, "Empty boolean argument can't be queried");
+	//testSet.addTest(&BoolTests::trailingCharacterTest, "Testing whether only values true or false are accepted");
 
 }
 
@@ -37,19 +38,28 @@ bool BoolTests::trueFalseTest(){
 }
 bool BoolTests::badInputTest(){
 	ArgList args;
-	args.push("program").push("-t").push("ahoj");
-	args.push("--false");
+	args.push("program").push("-t").push("abcd");
 	
 	FrontEnd arglib;
 	BoolType boolType = BoolType();
 	arglib.addOption("t", REQUIRED, boolType);
-	arglib.addOption("false", ALLOWED, boolType);
 
 	args.dump(cout);
 	
-	return Tests::parseMustThrow(arglib, args);//&&
-		//arglib.getOptionParameter<bool>("t") == true &&
-		//arglib.getOptionParameter<bool>("false") == false;
+	return Tests::parseMustThrow(arglib, args);
+}
+bool BoolTests::noInputTest(){
+	ArgList args;
+	args.push("program").push("--false");
+	
+	FrontEnd arglib;
+	BoolType boolType = BoolType();
+	string optionName = "false";
+	arglib.addOption(optionName, ALLOWED, boolType);
+
+	args.dump(cout);
+	return Tests::parseMustNotThrow(arglib, args)&&
+		Tests::getOptionValueMustThrow<bool>(arglib, "false"); 
 }
 bool BoolTests::trailingCharacterTest(){
 	// TODO: implement
