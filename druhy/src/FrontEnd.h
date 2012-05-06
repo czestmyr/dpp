1436c3a0c46319a4d07abe217f8e3286e2721945
Martin Bulant <bulisekspam@centrum.cz>
2012-05-06 10:43:56 +0200

    Added doxygen comments to methods in FrontEnd

--- druhy/src/FrontEnd.h
+++ druhy/src/FrontEnd.h
@@ -19,6 +19,16 @@ class FrontEnd {
 		~FrontEnd();
 
 		/// TODO: Document, what a "DerivedType" is
+		/// Adds new option witch all its properties specified by the user.
+		/// 
+		/// @param DefivedType Type specifiing which arguments can be added to the option.
+		///	It must be class derived from class Type.
+		/// @param optionName Name of option to be added to set of known options.
+		/// @param optionAttrib Spedifier defining if option is allowed or required.
+		///	Possible values are OPTION_ALLOWED and OPTION_REQUIRED.
+		/// @param paramType Allows the user to define type without any knowledge of templates. TODO: write better 
+		/// @param paramAttrib Specifier defining if option arguments are required, allowed or forbidden.
+		///	Possible values are PARAM_REQUIRED, PARAM_ALLOWED amd PARAM_FORBIDDEN
 		template <class DerivedType>
 		void addOption(
 			const std::string& optionName,
@@ -31,40 +41,85 @@ class FrontEnd {
 		}
 
 		/// Special case of addOption method when only option name is specified.
+		/// This method adds options which can't have any parameters.
 		/// This method has to be here because without a type specification, the compiler
 		/// does not know, which template instance of addOption it has to create.
+		/// @param optionName Name of option to be added to set of known options.
+		/// @param optionAttrib Spedifier defining if option is allowed or required.
+		///	Possible values are OPTION_ALLOWED and OPTION_REQUIRED.
+		///	Default value is OPTION_ALLOWED
 		void addOption(const std::string& optionName, OptionAttribute optionAttrib = OPTION_ALLOWED) {
-			addOption(optionName, optionAttrib, DummyType(), PARAM_FORBIDDEN);
+			addOptionInternal(optionName, optionAttrib, Type::getClone(DummyType()), PARAM_FORBIDDEN);
 		}
 
+		/// Defines synonyms between options.
+		/// The original option must be already defined.
+		/// Original can be previously defined synonym too.
+		/// Synonym can be in long or short form.
+		/// @param original Alredy specified option to which we want to add a synonym.
+		/// @param synonym Name of option which should be synonym for original.
 		void addSynonym(const std::string& original, const std::string& synonym);
 
-		void setOptionHelp(const std::string& option, const std::string& help);
+		/// Assignss help to specified option.
+		/// @param optionName Name of option to which add the help.
+		/// @param help Text of help to assign.
+		void setOptionHelp(const std::string& optionName, const std::string& help);
+
+		/// Writes help to all specified options and format it to desired width.
+		/// @param stream Stream to which should be the help written.
+		/// @param terminalSize Width of the terminal in characters.
+		///	The help will be wrapped to this number of characters.
 		void writeHelp(std::ostream& stream, int terminalSize = 80) const;
+
+		/// Parse arguments stored in @param argv.
+		/// @param argc Number of tokens in argv.
+		/// @param argv Command line parameters given to the program.
+		/// 	They have to start with name of the program,
+		///	because this is common for agguments in C++.
 		void parse(int argc, const char* argv[]);
 
+		/// Tests if the option @param optionName was given.
+		/// @param optionName Name of the option to be tested if given.
+		/// @return If option optionName was given.
 		bool isOptionSet(const std::string& optionName) const;
 
-		// TODO: What to do with the cast exception?
+		/// This method gets parameter of option specified by optionName.
+		/// It returns the value of the argument transformed to ValueType specified by the user.
+		/// @param ValueType Specifies a type of return value expected by the user.
+		/// @param optionName Name of the option to which return the argument. 
+		/// @return Value of the argument casted to type ValueType
 		template <class ValueType>
 		ValueType getOptionParameter(const std::string& optionName) const {
 			unsigned int id = syntax->getId(optionName);
 			ValueHandle valueHandle = data->getOptionParameter(id);
-		
 			if (valueHandle.isEmpty()) {
-				// TODO: throw exception
+				throw ArgumentException("No parametr were added to this option.");
 			}
-
+		
 			return valueHandle.getValue<ValueType>();
 		}
 
+		/// Returns all regular arguments parsed in last call of function parse.
+		/// @return reference to regular arguments stored in vector of strings 
 		const std::vector<std::string>& getRegularArguments() const;
 
 	private:
-		void addOptionInternal(const std::string& optionName, OptionAttribute optionAttrib, Type* paramType, ParameterAttribute attrib);
-
-		OptionSyntax* syntax;	
-		ArgumentData* data;
+		/// Separates adding new option in frontEnd and our internal representations of option syntax.
+		/// @param optionName Name of option to be added in set of known options.
+		/// @param optionAttrib Spedifier defining if option is allowed or required.
+		///	Possible values are OPTION_ALLOWED and OPTION_REQUIRED.
+		/// @param paramType Specifies a type of parameter. This type must be derived from type Type. 
+		/// @param paramAttrib Specifier defining if option arguments are required, allowed or forbidden.
+		///	Possible values are PARAM_REQUIRED, PARAM_ALLOWED amd PARAM_FORBIDDEN
+		void addOptionInternal(
+			const std::string& optionName, 
+			OptionAttribute optionAttrib, 
+			Type* paramType, 
+			ParameterAttribute paramAttrib
+		);
+		
+		OptionSyntax* syntax; /// Stores syntax of options defined by the user.
+		ArgumentData* data; /// Stores all regular arguments given in parameters.
 };
 
 #endif
--- druhy/src/values/IntValue.cpp
+++ druhy/src/values/IntValue.cpp
@@ -2,13 +2,14 @@
 #include "../ArgumentException.h"
 
 //TODO: Add proper information to this exception!
+//TODO: change test valuePtr == NULL za isEmpty()
 template <> int ValueHandle::getValue<int>() {
-		if( valuePtr == NULL ) {
+		if (valuePtr == NULL) {
 			throw ArgumentException("No parametr were added to this option.");
 		}
 
 		IntValue* intValue = dynamic_cast<IntValue*>(valuePtr);
-		if( intValue == NULL ) {
+		if (intValue == NULL) {
 			throw ArgumentException("Type defined with this option doesn't match type required by this function.");
 		}
 		return intValue->get();
