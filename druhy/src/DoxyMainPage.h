5cdc17863e959e94b5921e14ed150aeab93198ee
Cestmir Houska <czestmyr@gmail.com>
2012-05-06 17:55:25 +0200

    Added frontpage stub for doxy
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

---
+++ druhy/src/DoxyMainPage.h
@@ -0,0 +1,17 @@
+// Prevent including this file. It only serves as a place for the front-page documentation in doxygen.
+#error This file should not be included anywhere. It does not contain anything useful
+/*! \mainpage Arglib library documentation
+ * 
+ * \section intro_sec Introduction
+ *
+ * Arglib is a university project for the course Recommended Programming Practices at the Charles University in Prague.
+ * It is a command-line parsing library that supports short and long format options with or without parameters as well
+ * as regular arguments (i.e. not options). The parameters can have any of the built-in types - see Type hierarchy.
+ * If that was not enough, the user can use inheritance to create new types. The library was created by Čestmír Houška
+ * and Martin Bulant.
+ *
+ * \section install_sec Installation
+ * \subsection step1 Step 1: Opening the box
+ * etc...
+ */
+
