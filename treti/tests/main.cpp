f6e1eff8e6386f9162ab486796407ecaffd814e8
Cestmir Houska <czestmyr@gmail.com>
2012-05-28 13:52:42 +0200

    Test makefile now links the library
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- treti/tests/main.cpp
+++ treti/tests/main.cpp
@@ -5,8 +5,11 @@
 #include <vector>
 
 using namespace std;
+using namespace options;
 
 int main (int arc, char* argv[]) {
+  option_category blah;
+
   Tests::Tests tests;
   return tests.runTests();
 }
